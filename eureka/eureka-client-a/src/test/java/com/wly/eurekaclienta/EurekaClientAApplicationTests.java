cb679c0642743f33e593fd176b697dddebb8a121
wly <wlywly0735@126.com>
2023-11-03 23:11:29 +0800

    eureka_init

---
+++ eureka/eureka-client-a/src/test/java/com/wly/eurekaclienta/EurekaClientAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wly.eurekaclienta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
